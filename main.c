40a760eab2f46aae18fcf1d44dd6abb1e3ad94d7
Betobit <jesusmartinoza@gmail.com>
2015-09-10 22:44:02 -0500

    Estilo y botones agregados

--- main.c
+++ main.c
@@ -3,40 +3,41 @@
 #include <stdlib.h>
 
 #define CIRCULO_TAM 14
-#define HEIGHT 640
-#define WIDTH 480
+#define HEIGHT 600
+#define WIDTH 800
 
+typedef char String[50];
 typedef struct {
     int color;
     int x;
     int y;
 }TColor;
 
+typedef struct {
+    int x;
+    int y;
+}TBoton;
+
 // AREA DE COLORES
 void ColoresPrincipales(int xInicial, int yInicial);
 void ColoresDinamicos(int xInicial, int yInicial, int *color);
+void CreaUI();
+
+// MENU
+void CreaMenu(TBoton *b);
 
 int main()
 {
-    initwindow(WIDTH,HEIGHT,"Editor Sprite");
-    setfillstyle(1,COLOR(44, 62, 80));
-    setcolor(COLOR(44, 62, 80));
-    bar(0, 0, WIDTH, HEIGHT);
-    setlinestyle(0, 0, 6);
-    int x = WIDTH-40,
-        y = HEIGHT/10,
-        color[] = { 101, 32, 60 };
-    ColoresPrincipales(x-50, y);
+    CreaUI();
     while(true)
     {
         while(!ismouseclick(WM_LBUTTONDOWN));
         int xm, ym;
         getmouseclick(WM_LBUTTONDOWN, xm, ym);
-        int color1 = getpixel(xm, ym);
-        setcolor(color1);
-        rectangle(0, 0, WIDTH/2, HEIGHT/2);
-        ColoresDinamicos(x, y, &color1);
-
+        int colorSel = getpixel(xm, ym);
+        setcolor(colorSel);
+        rectangle(40, HEIGHT/10-CIRCULO_TAM, WIDTH-190, HEIGHT-90);
+        ColoresDinamicos(WIDTH-50, HEIGHT/10, &colorSel);
     }
     getch();
     return (0);
@@ -78,15 +79,15 @@ void ColoresPrincipales(int xInicial, int yInicial)
         setfillstyle(1, colores[i]);
         if(i==9)
         {
-            xInicial -= 50;
+            xInicial += 50;
             yInicial -= (CIRCULO_TAM/2*5)*9;
         }
         for(j=0; j<CIRCULO_TAM; j++)
         {
             fillellipse(xInicial, yInicial, j, j);
-            delay(10);
+            //delay(6);
         }
-        delay(9);
+        //delay(4);
         yInicial += CIRCULO_TAM/2*5;
     }
 }
@@ -108,5 +109,34 @@ void ColoresDinamicos(int xInicial, int yInicial, int *color)
         }
         delay(10);
         yInicial += CIRCULO_TAM/2*5;
+    }
+}
+
+void CreaUI()
+{
+    initwindow(WIDTH,HEIGHT,"Editor de Sprites");
+    setfillstyle(1,COLOR(44, 62, 80));
+    setcolor(COLOR(44, 62, 80));
+    bar(0, 0, WIDTH, HEIGHT);
+    setlinestyle(0, 0, 6); // Crea efecto de mancha en los colores.
+    ColoresPrincipales(WIDTH-150, HEIGHT/10);
+    TBoton botones[4];
+    CreaMenu(botones);
+}
+
+void CreaMenu(TBoton *b)
+{
+    char i;
+    int xi = 50;
+    String opciones[] = {"Nuevo", "Abrir", "Guardar", "Salir" };
+    setbkcolor(0x0066F4);
+    setfillstyle(1, 0x0066F4);
+    setcolor(0x003988);
+    settextstyle(2, HORIZ_DIR, 8);
+    for(i=0; i<sizeof(opciones)/50; i++)
+    {
+        bar( (b+i)->x = xi-10, HEIGHT-textheight("A")*2-10, xi+textwidth(opciones[i])+10, HEIGHT);
+        outtextxy(xi, HEIGHT-textheight("A")*2, opciones[i]);
+        xi += textwidth(opciones[i])+40;
     }
 }
